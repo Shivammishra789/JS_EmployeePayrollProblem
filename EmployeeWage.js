5e211377624da1722511a3e91161b6dc0ecf30ed
Shivammishra789 <shivammishra7789@gmail.com>
2021-11-24 17:21:12 +0530

    ADDED: check if employee is present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,9 @@
-console.log("Welcome to employee pay roll ")
+//UC1
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random()*2);
+if(empCheck == IS_ABSENT){
+    console.log("Employee is Absent");
+}
+else{
+    console.log("Employee is present");
+}
