8eb7dd2006bc83627c9c5f94b30b07d33cabfda3
Shivammishra789 <shivammishra7789@gmail.com>
2021-11-25 20:49:05 +0530

    ADDED: calculated employee wage for 20 days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -14,14 +14,16 @@ function getWorkingHours (empCheck) {
             return FULL_TIME_HOURS;
         default:
             return 0;
-        }
     }
-    let empHrs = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs = getWorkingHours (empCheck);   // calling function to get working hours
-    let empWage = empHrs * WAGE_PER_HOUR;
-    console.log("Emp Wage: "+ empWage);
-    
+}   
 
+const NUM_OF_WORKING_DAYS = 20;
+let empHrs = 0;
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours (empCheck);     // calling function to get working hours
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hrs: " +empHrs+ " Emp Wage: " + empWage);
 
 
